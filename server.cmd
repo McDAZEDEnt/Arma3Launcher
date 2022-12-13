f245eb8fc85053a570cbace481dd24aaef959463
Shadow <mattmacdonald112@gmail.com>
2022-12-13 14:54:44 -0800

    commit
    Former-commit-id: 4a6c066b34286d7c486f957004da0d6a3bd05656

--- server.cmd
+++ server.cmd
@@ -5,6 +5,5 @@ start steam://rungameid/
 echo Hit enter once steam has logged in...
 pause
 call powershell "& '.\launch.ps1'"
-call powershell "& '.\launchserver.ps1'"
 call powershell "& '.\launchclient.ps1'"
 call powershell "& '.\launchclient.ps1'"
