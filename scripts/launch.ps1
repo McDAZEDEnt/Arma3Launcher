8c364a555edb630c62e1d6871c792a42c11dbb1b
Shadow <mattmacdonald112@gmail.com>
2022-12-27 08:59:26 -0800

    commit

--- scripts/launch.ps1
+++ scripts/launch.ps1
@@ -15,7 +15,7 @@ $ErrorActionPreference = "Stop"
 $a3RootPath = "$env:arma3/"
 $serverExeName = "arma3_x64.exe"
 $port = 2302
-$flags = "-skipIntro -noSplash -hugePages -filePatching -setThreadCharacteristics -cpuCount=4 -exThreads=7 -malloc=system -limitFPS=75"
+$flags = "-skipIntro -noSplash -hugePages -filePatching -setThreadCharacteristics -cpuCount=4 -exThreads=7 -malloc=system -limitFPS=60"
 
 # Relative paths to server config files
 $presetsFolder = "..\presets\"
