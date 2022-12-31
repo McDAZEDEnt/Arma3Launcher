1d8f5acb59d29258efe40d2f5cae1b4db486dcc6
Shadow <mattmacdonald112@gmail.com>
2022-12-31 10:43:14 -0800

    commit

--- scripts/launchhc.ps1
+++ scripts/launchhc.ps1
@@ -15,7 +15,7 @@ $ErrorActionPreference = "Stop"
 $a3RootPath = "$env:arma3/"
 $serverExeName = "arma3server_x64.exe"
 $port = 2302
-$flags = "-client -nosound -connect=localhost -password=1969 -hugePages -filePatching -setThreadCharacteristics -exThreads=7 -malloc=system -limitFPS=60"
+$flags = "-client -nosound -connect=localhost -password=1969 -hugePages -filePatching -setThreadCharacteristics -exThreads=7 -malloc=system -limitFPS=75"
 
 # Relative paths to server config files
 $presetsFolder = "..\presets\"
